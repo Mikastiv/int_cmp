e042b40532919d3833308f5989906b8b6a6d9da8
Mikastiv <m.leblanc_3@hotmail.com>
2020-09-30 19:48:24 -0400

    integer trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-pub mod u8_i8;
 pub mod u8_i16;
+pub mod u8_i8;
 
 /// Comparison trait for integer types
-pub trait IntCmp<Other: Copy>: Copy {
+pub trait IntCmp<Other: Integer> {
     fn cmp_eq(self, other: Other) -> bool;
 
     fn cmp_ne(self, other: Other) -> bool;
@@ -16,4 +16,15 @@ pub trait IntCmp<Other: Copy>: Copy {
     fn cmp_lt(self, other: Other) -> bool;
 }
 
+pub trait Integer {}
 
+impl Integer for u8 {}
+impl Integer for u16 {}
+impl Integer for u32 {}
+impl Integer for u64 {}
+impl Integer for u128 {}
+impl Integer for i8 {}
+impl Integer for i16 {}
+impl Integer for i32 {}
+impl Integer for i64 {}
+impl Integer for i128 {}
